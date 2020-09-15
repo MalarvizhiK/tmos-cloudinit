53e142ec7f878cdbde28f478bc1e83a09e2f1c31
MalarvizhiK <k.malarvizhi@in.ibm.com>
2020-09-15 12:38:07 +0530

    Update compute.tf

--- demo/ibmcloud_vpc_gen2/schematics/tmos_multi_nic/compute.tf
+++ demo/ibmcloud_vpc_gen2/schematics/tmos_multi_nic/compute.tf
@@ -67,7 +67,7 @@ locals {
 locals {
   # custom image takes priority over public image
   # image_id = data.ibm_is_image.tmos_custom_image.id == null ? lookup(local.public_image_map[var.tmos_image_name], var.region) : data.ibm_is_image.tmos_custom_image.id
-  image_id = "r006-1ca34358-b1f0-44b1-bf9a-a8bd9837a672"
+  image_id = lookup(local.public_image_map["bigip-14-1-2-6-0-0-2-all-1slot"], var.region)
   # public image takes priority over custom image
   # image_id = lookup(lookup(local.public_image_map, var.tmos_image_name, {}), var.region, data.ibm_is_image.tmos_custom_image.id)
   template_file = lookup(local.license_map, var.license_type, local.license_map["none"])
